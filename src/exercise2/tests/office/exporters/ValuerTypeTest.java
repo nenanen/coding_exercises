b91308857797e47a89614980ed36b2e4da2960a8
nenaodriscoll <nena96@live.nl>
2024-12-22 13:32:59 +0100

    exercise 2: add ValueType test

---
+++ src/exercise2/tests/office/exporters/ValuerTypeTest.java
@@ -0,0 +1,36 @@
+package exercise2.tests.office.exporters;
+
+import exercise2.main.office.Office;
+import exercise2.main.office.SpreadSheetImpl;
+import exercise2.main.office.ValueType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ValuerTypeTest {
+
+    @Test
+    public void recognizesIntegers() {
+        Assert.assertEquals(ValueType.INTEGER, ValueType.ofString("-1"));
+        Assert.assertEquals(ValueType.INTEGER, ValueType.ofString("999"));
+    }
+
+    @Test
+    public void recognizesFormulas() {
+        Assert.assertEquals(ValueType.FORMULA, ValueType.ofString("=1+2"));
+        Assert.assertEquals(ValueType.FORMULA, ValueType.ofString("=    1 % 5"));
+    }
+
+
+    @Test
+    public void recognizesStrings() {
+        Assert.assertEquals(ValueType.STRING, ValueType.ofString(""));
+        Assert.assertEquals(ValueType.STRING, ValueType.ofString("'123"));
+        Assert.assertEquals(ValueType.STRING, ValueType.ofString("' = 123'"));
+        Assert.assertEquals(ValueType.STRING, ValueType.ofString("hello"));
+    }
+}
+
