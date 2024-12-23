7dabc8ffdf9b4fbc7f6e377f4e1b2213c83f5a78
nenaodriscoll <nena96@live.nl>
2024-12-23 18:15:13 +0100

    add small comment

--- src/main/java/exercise1/main/Deduplicator.java
+++ src/main/java/exercise1/main/Deduplicator.java
@@ -13,9 +13,8 @@ public class Deduplicator {
      * @return a list of duplicate elements, sorted by their initial occurrence in the input
      */
     public static List<String> getDuplicatesInOriginalOrder(List<String> elements) {
-        Map<String, Integer> firstOccurrences = new HashMap<>();
-        Map<String, Integer> duplicates = new HashMap<>();
-
+        Map<String, Integer> firstOccurrences = new HashMap<>(); // stores an element and the index of its first occurrence
+        Map<String, Integer> duplicates = new HashMap<>(); // stores a duplicate element and the index of its first occurrence
 
         for (int i = 0; i < elements.size(); i++) {
             String element = elements.get(i);
@@ -27,7 +26,7 @@ public class Deduplicator {
         }
 
         return duplicates.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
+                .sorted(Map.Entry.comparingByValue()) // sort the elements by the value aka the index of its first occurrence
                 .map(Map.Entry::getKey)
                 .toList();
     }
